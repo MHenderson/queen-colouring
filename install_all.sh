f3ce46ec3f76af24ac98eec541a7990d6db75ed9
Matthew Henderson <matthew.james.henderson@gmail.com>
2022-09-22 13:01:56 +0000

    add a script for installing all system dependencies

--- install_all.sh
+++ install_all.sh
@@ -7,3 +7,12 @@ wget --no-check-certificate http://www.cs.ualberta.ca/%7Ejoe/Coloring/Colorsrc/c
 tar xzvf color.tar.gz
 mkdir bin
 make all
+
+cd /opt
+git clone https://github.com/docopt/docopts.git
+cd docopts
+./get_docopts.sh
+cp docopts /usr/local/bin
+
+apt-get install expect
+cp -r /home/matthew/workspace/ccli /opt
