76215e9db1973911426f374cc1871d44b58ff769
Matthew Henderson <matthew.james.henderson@gmail.com>
2022-09-22 13:03:42 +0000

    don't really need these any more

--- install_ccli.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-apt-get install expect
-cp -r /home/matthew/workspace/ccli /opt
--- install_docopts.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd /opt
-git clone https://github.com/docopt/docopts.git
-cd docopts
-./get_docopts.sh
-cp docopts /usr/local/bin
