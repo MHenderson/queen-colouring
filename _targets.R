59c8c1632c95017f2c97a355e447b8e559fce556
Matthew Henderson <matthew.james.henderson@gmail.com>
2025-04-15 19:34:39 +0100

    Move code from functions into main pipeline.

--- _targets.R
+++ _targets.R
@@ -10,18 +10,63 @@ tar_source()
 list(
   tar_target(
        name = experiments,
-    command = generate_experiments(n_iter = 20, orders = 5:16, seed = 42)
+    command = {
+      
+      n_iter <- 20
+      orders <- 5:16
+      seed <- 42
+
+      set.seed(seed)
+
+      seeds <- sample(1:100000, n_iter)
+      files <- glue("queen{orders}_{orders}.col") |> as.character()
+
+      list(
+        problem_file = here("graphs", files),
+                type = c("simple", "large", "small", "random"),
+            ordering = c("inorder", "random", "decdeg", "incdeg"),
+                seed = seeds,
+               cheat = c(TRUE, FALSE),
+               kempe = c(TRUE, FALSE)
+      ) |> cross_df()
+    }
+
   ),
   tar_target(
        name = results,
-    command = run_experiments(experiments)
+    command = {
+      experiments |>
+        mutate(
+          colouring = pmap(experiments, ccli_greedy_colouring)
+        )
+    }
   ),
   tar_target(
        name = results2,
-    command = compute_n_colours(results)
+    command = {
+      results |>
+        mutate(
+                  n = as.numeric(str_match(problem_file, "([0-9]+).col")[,2]),
+          n_colours = map_dbl(colouring, max)
+        )
+    }
   ),
   tar_target(
        name = plot,
-    command = plot_results(results2)
+    command = {
+      results2 |>
+        group_by(n) |>
+        slice_max(n_colours) |>
+        group_by(n) |>
+        slice(1) |>
+        ggplot(aes(n, n_colours)) +
+          geom_point() +
+          geom_text(hjust = -.2, vjust = .8, aes(label = paste0("(", n, ",", n_colours, ")"))) +
+          geom_abline(colour = "blue", alpha = .2, slope = 1, intercept = 0) +
+          ylim(0, 30) +
+          xlim(0, 30) +
+          theme_minimal()
+
+    }
   )
 )
